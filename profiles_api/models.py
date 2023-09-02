80e77745a3c56d7f27f4d9a68809f3d1742e76a1
ahmedafeef <ahmedafeef1999@gmail.com>
2023-09-02 08:23:04 +0300

    Enable Django akmin for user profile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -14,7 +14,7 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name, )
+        user = self.model(email=email, name=name)
 
         user.set_password(password)
         user.save(using=self._db)
@@ -35,13 +35,14 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255, default="")
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELD = ["name"]
+    REQUIRED_FIELDS = ["name"]
 
     def get_full_name(self):
         """Retrieve full name for user"""

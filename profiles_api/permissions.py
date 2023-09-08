c3b0ca149aef8a8ee81b59db1875a8d1138173ef
ahmedafeef <ahmedafeef1999@gmail.com>
2023-09-08 06:15:23 +0300

    added feed items to the profile users and permission, authentication its

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -3,10 +3,20 @@ from rest_framework import permissions
 class UpdateOwnProfile(permissions.BasePermission):
     """Allow users to update their own profile"""
 
-    def has_object_permission(self, request,  view, object):
+    def has_object_permission(self, request,  view, obj):
         """Check if the users trying to edit their own profile"""
 
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return object.id == request.user.id
+        return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
